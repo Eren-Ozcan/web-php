d5cd6c01bd01840bd181389a86da5df389c561fc
Eren Özcan <erenozcaan@hotmail.com>
2025-07-21 03:23:19 +0300

    aa

--- server/db.js
+++ server/db.js
@@ -3,8 +3,8 @@ import mysql from 'mysql2/promise';
 export const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'admin',
-  password: process.env.DB_PASS || 'merhaba123',
-  database: process.env.DB_NAME || 'mefaalum_wp289',
+  password: process.env.DB_PASS || '1234',
+  database: process.env.DB_NAME || 'mefaaluminyum_wp289',
   waitForConnections: true,
   connectionLimit: 10
 });
